68649b44e169d91699c2e072d04ddf5dc8d981fb
amahmed81 <40702324+amahmed81@users.noreply.github.com>
2025-09-04 11:01:02 +0000

    Commented the ec2 destry

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "eu-west-2"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
+/*resource "aws_instance" "example" {
     ami           = "ami-044415bb13eee2391"  # Specify an appropriate AMI ID
     instance_type = "t3.micro"
-}
+}*/
